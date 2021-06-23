b0cd3b30b9e5b7d42ed485f968b6e955d4454ea0
Cristhian <yurday93@hotmail.com>
2021-06-22 19:14:49 -0500

    Creación de los constructores y operaciones bancarias

--- src/main/java/StartApp.java
+++ src/main/java/StartApp.java
@@ -1,15 +1,18 @@
-import java.beans.Statement;
+import operations.Account;
+import operations.Statement;
+
+import static builders.DateCreator.date;
+import static operations.Amount.amountOf;
 
 public class StartApp {
-
-    public static void main(String[] args) {
-        Account account = new Account(new Statement());
-
-        account.deposit(amountOf(1000), date("10/01/2021"));
-        account.deposit(amountOf(2000), date("13/01/2021"));
-        account.withdrawal(amountOf(500), date("14/01/2021"));
-
-        account.printStatement(System.out);
-    }
-
+	
+	public static void main(String[] args) {
+		Account account = new Account(new Statement());
+		
+		account.deposit(amountOf(1000), date("10/01/2012"));
+		account.deposit(amountOf(2000), date("13/01/2012"));
+		account.withdrawal(amountOf(500), date("14/01/2012"));
+		
+		account.printStatement(System.out);
+	}
 }
